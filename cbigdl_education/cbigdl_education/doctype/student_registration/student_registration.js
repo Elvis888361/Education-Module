aec8e81ec630435ac319be68b48c62c3c7f8cf1f
GKDevs <gkdevs50@gmail.com>
2023-06-10 20:23:51 +0300

    Class Added

--- cbigdl_education/cbigdl_education/doctype/student_registration/student_registration.js
+++ cbigdl_education/cbigdl_education/doctype/student_registration/student_registration.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Student Registration', {
-	// refresh: function(frm) {
-
-	// }
+    student_group: function(frm) {
+        cur_frm.fields_dict['class'].get_query = function(doc) {
+            return {
+                filters: {
+                    "parent": frm.doc.student_group,
+                }
+            }
+        }
+    }
 });
